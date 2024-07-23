9834aaa9dada54167d11d0887faf3f49c8b9bedb
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-23 19:19:45 +0300

    add api agent

---
+++ src/api/agent.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosResponse } from 'axios';
+import TaskDto from '../components/TaskForm/modals/TaskDto';
+import { Task } from '../components/ToDoList/models/Task';
+import { Status } from '../components/ToDoList/models/Status';
+
+const agent = axios.create({
+    baseURL: 'https://localhost:44331/',
+    transformResponse: (data) => JSON.parse(data),
+    headers: {
+        Accept: 'application/json'
+      },
+  });
+
+const responseData = <T> (response: AxiosResponse<T>) => response.data;
+
+export const getAllTasks = async (): Promise<Task[]> => agent.get<Task[]>('/tasks').then(responseData);
+
+export const getAllStatuses = async ():Promise<Status[]> => agent.get<Status[]>('/statuses').then(responseData);
+
+export const createTask = (dto: TaskDto): Promise<Task> => agent.post<Task>('/tasks', dto).then(responseData)
+
+export const updateTask = async (dto: TaskDto) => agent.put('/tasks', dto)
+
+export const deleteTask = async (id: number) => agent.delete('/tasks')
--- src/components/ToDoList/models/Task.tsx
+++ src/components/ToDoList/models/Task.tsx
@@ -2,14 +2,14 @@ import { Status } from "./Status"
 
 export class Task{
     id: number
-    name: string
+    title: string
     description: string
     status: Status
     priority: number
 
     constructor(id: number, name: string, description: string, priority: number, status: Status){
         this.id = id
-        this.name = name
+        this.title = name
         this.description = description
         this.status = status
         this.priority = priority
