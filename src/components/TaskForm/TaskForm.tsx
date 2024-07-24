4fce226e286e141da278575950db5afb09b90fab
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-24 11:55:43 +0300

    add errors

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -36,7 +36,7 @@ export const TaskForm: FunctionComponent<Props> = ({submitButtonName, submitActi
         statusId: ''
     })
 
-    const [isFormValid, setValidity] = useState(true)
+    const [isFormValid, setValidity] = useState<boolean>(true)
     const [validationErrors, setErrors] = useState<TaskFormValidationErrors>({
         titleError: undefined,
         descriptionError: undefined,
@@ -61,7 +61,7 @@ export const TaskForm: FunctionComponent<Props> = ({submitButtonName, submitActi
         setFormData({...formData, [name]: value})
     }
 
-    const validateForm = () => {
+    const validateForm = ():boolean => {
         let isValid = true;
         const errors: TaskFormValidationErrors = {
             titleError: undefined,
@@ -69,7 +69,12 @@ export const TaskForm: FunctionComponent<Props> = ({submitButtonName, submitActi
             priorityError: undefined,
             statusError: undefined
         }
-        if(formData.title.length < 5 || formData.title.length > 30){
+
+        if(formData.title.trim().length == 0){
+            isValid = true;
+            errors.titleError = "The title is required"
+        }
+        else if(formData.title.length < 5 || formData.title.length > 30){
             isValid = false;
             errors.titleError= "The length of title must be more than 5 and less than 30 characters"
         }
@@ -79,7 +84,11 @@ export const TaskForm: FunctionComponent<Props> = ({submitButtonName, submitActi
             errors.descriptionError = "Description is required"
         }
 
-        if(isNaN(Number(formData.priority))){
+        if(formData.priority.trim().length === 0){
+            isValid = false;
+            errors.priorityError = "Priority is required"
+        }
+        else if(isNaN(Number(formData.priority))){
             isValid = false;
             errors.priorityError = "Priority must be a number"
         }
@@ -88,18 +97,16 @@ export const TaskForm: FunctionComponent<Props> = ({submitButtonName, submitActi
             errors.priorityError = "Priority must be greater than 0"
         }
 
-        if(isNaN(Number(formData.statusId))){
+        if(isNaN(Number(formData.statusId)) || formData.priority.trim().length === 0){
             isValid = false
             errors.statusError = "Status is required"
         }
-
-        setValidity(isValid)
         setErrors(errors)
+        return isValid;
     }
 
-    const handleSubmit = () => {
-        validateForm()
-        if(isFormValid === true){
+    const handleSubmit = async () => {
+        if(validateForm()){
             submitAction(
             {
                 title: formData.title,
@@ -113,14 +120,22 @@ export const TaskForm: FunctionComponent<Props> = ({submitButtonName, submitActi
     return (
         <div className="TaskForm">
             <input className="TaskFormInput" placeholder="Title" value={formData.title} name='title' onChange={handleInputChange}/>
+            <div className="TaskFormError">{validationErrors.titleError}</div>
+
             <input className="TaskFormInput" placeholder="Priority" value={formData.priority} name='priority' onChange={handleInputChange}/>
+            <div className="TaskFormError">{validationErrors.priorityError}</div>
+
             <select className="TaskFormSelect" value={formData.statusId} name='statusId' onChange={handleInputChange}>
                 <option value="" disabled selected hidden>Status</option>
                 {taskStore.statuses.map( (item) => <option value={item.id} key={item.id}>{item.name}</option>)}
             </select>
+            <div className="TaskFormError">{validationErrors.statusError}</div>
+
             <textarea className="TaskFormTextArea" id="" placeholder="Description" value={formData.description} name='description' onChange={handleInputChange}>
 
             </textarea>
+            <div className="TaskFormError">{validationErrors.descriptionError}</div>
+
             <button className="TaskFormButton" onClick={handleSubmit}>{submitButtonName}</button>
         </div>
                 
