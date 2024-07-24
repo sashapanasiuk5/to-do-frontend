d590389d11b87c71ea922b13d0d2fa4168e95357
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-24 18:45:46 +0300

    make taskItem draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ToDoList from './components/ToDoList/ToDoList';
 import { StoreContext, store} from './stores/store'
+import {DndContext, PointerSensor, TouchSensor, useSensor, useSensors} from '@dnd-kit/core';
+import CreateTaskModal from './components/modals/CreateTaskModal/CreateTaskModal';
+import OpenTaskModal from './components/modals/OpenTaskModal/OpenTaskModal';
+import EditTaskModal from './components/modals/EditTaskModal/EditTaskModal';
 
 function App() {
   return (
     <div className="App">
-      <StoreContext.Provider value={store}>
-        <ToDoList/>
+      <StoreContext.Provider value={store} >
+          <ToDoList/>
+          <CreateTaskModal/>
+          <OpenTaskModal/>
+          <EditTaskModal/>
       </StoreContext.Provider>
     </div>
   );
--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -6,15 +6,19 @@ import { ToDoListItem}  from '../ToDoListItem/ToDoListItem';
 import './ToDoList.css'
 import { observer } from 'mobx-react-lite';
 import useMobx from '../../stores/store';
-import CreateTaskModal from '../modals/CreateTaskModal/CreateTaskModal';
-import OpenTaskModal from '../modals/OpenTaskModal/OpenTaskModal';
-import EditTaskModal from '../modals/EditTaskModal/EditTaskModal';
-import ConfirmationModal from '../modals/ConfrmationModal/ConfirmationModal';
+import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
 
 
 function ToDoList(){
     const { taskStore, modalsStore} = useMobx();
-
+    const sensors = useSensors(
+        useSensor(PointerSensor, {
+          activationConstraint:{
+            distance: 10
+          }
+        }),
+      );
+      
     useEffect(()=> {
         taskStore.fetchStatusesAsync()
         taskStore.fetchTasksAsync()
@@ -32,38 +36,37 @@ function ToDoList(){
     }
     
     return (
-        <div className="ToDoListWrapper">
-            <div className='list-wrapper'>
-                <div className="column">
-                    <div className="column-title">
-                        To do
+        <DndContext sensors={sensors}>
+            <div className="ToDoListWrapper">
+                <div className='list-wrapper'>
+                    <div className="column">
+                        <div className="column-title">
+                            To do
+                        </div>
+                        <div className="todos">
+                            {GetTasksByStatus('to-do')}
+                        </div>
                     </div>
-                    <div className="todos">
-                        {GetTasksByStatus('to-do')}
+                    <div className="column">
+                        <div className="column-title">
+                            In progress
+                        </div>
+                        <div className="todos">
+                            {GetTasksByStatus('in-progress')}
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="column-title">
+                            Done
+                        </div>
+                        <div className="todos">
+                            {GetTasksByStatus('done')}
+                        </div>
                     </div>
                 </div>
-                <div className="column">
-                    <div className="column-title">
-                        In progress
-                    </div>
-                    <div className="todos">
-                        {GetTasksByStatus('in-progress')}
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="column-title">
-                        Done
-                    </div>
-                    <div className="todos">
-                        {GetTasksByStatus('done')}
-                    </div>
-                </div>
+                <button className='ToDoListAddButton' onClick={handleAddButtonClick}></button>
             </div>
-            <CreateTaskModal/>
-            <OpenTaskModal/>
-            <EditTaskModal/>
-            <button className='ToDoListAddButton' onClick={handleAddButtonClick}></button>
-        </div>
+        </DndContext>
     );
 }
  
